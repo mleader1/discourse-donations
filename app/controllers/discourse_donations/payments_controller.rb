3eb85870b207e7c85f8c4a09f9d036de37b7cec7
Chris Beach <chris@chrisbeach.co.uk>
2017-11-08 13:16:32 +0000

    WIP

--- app/controllers/discourse_donations/payments_controller.rb
+++ app/controllers/discourse_donations/payments_controller.rb
@@ -1,11 +1,21 @@
 module DiscourseDonations
   class PaymentsController < ApplicationController
     def index
-      render json: {}
+      render html: {}
     end
 
     def show
-      render json: {}
+      return redirect_to path('/login') if SiteSetting.login_required? && current_user.nil?
+
+      @about = About.new
+      respond_to do |format|
+        format.html do
+          render :index
+        end
+        format.json do
+          render_serialized(@about, AboutSerializer)
+        end
+      end
     end
   end
 end
